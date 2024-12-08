2aac28c9045172e812bc7d626d8aacfb1b1116f9
Sachitha Hirushan <shpsachitha@gmail.com>
2024-12-08 19:28:02 +0530

    added docs

--- web_app/babel.config.js
+++ web_app/babel.config.js
@@ -1,6 +1,4 @@
 const isTest = process.env.NODE_ENV === "test";
-console.log("hellow test")
-console.log(isTest)
 module.exports = {
   presets: isTest
     ? ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
